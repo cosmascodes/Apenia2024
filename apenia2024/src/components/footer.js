a843f05391c2c29dead2cb20e63b69963c17aed5
cosmascodes <56020081+cosmascodes@users.noreply.github.com>
2024-03-16 00:38:37 +0300

    push

--- apenia2024/src/components/footer.js
+++ apenia2024/src/components/footer.js
@@ -1,12 +1,26 @@
 const Footer = () => {
   return (
-    <footer className="bg-blackTheme bg-hero-wave bg-cover bg-center py-24 px-6 text-white flex flex-col justify-center items-center">
-      <div className=" w-2/3 text-center ">
+    <footer className="bg-blackTheme bg-hero-wave bg-cover bg-center pt-24 px-6 text-white flex flex-col justify-center items-center">
+      <div className=" w-2/3 text-center flex flex-col items-center">
         <h1 className=" font-bold text-7xl ">
           Elevate Your Finances With Divi
         </h1>
-        <button className="bg-primary uppercase text-white rounded-full px-10 py-4 text-sm mt-8">
+        <button className="bg-primary uppercase text-white rounded-full px-10 py-4 text-sm mt-8 flex flex-row">
           Join Apenia today
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            class="w-5 h-5 ml-5"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m8.25 4.5 7.5 7.5-7.5 7.5"
+            />
+          </svg>
         </button>
       </div>
       <div className="grid grid-cols-4 w-full place-items-center mt-14">
@@ -150,6 +164,9 @@ const Footer = () => {
             </li>
           </ul>
         </div>
+      </div>
+      <div className="pt-24 pb-10">
+        <p>&copy; 2024 Apenia Technologies Ltd. All rights reserved.</p>
       </div>
     </footer>
   );
