de9bdf2cf0dbaadc2e7fef9f105c23911a4c2a06
cosmascodes <56020081+cosmascodes@users.noreply.github.com>
2024-03-16 01:58:22 +0300

    push

--- apenia2024/src/app/page.js
+++ apenia2024/src/app/page.js
@@ -3,7 +3,44 @@ import Hero from "@/sections/landing/hero";
 export default function Home() {
   return (
     <main className="bg-secondary">
-      <Hero />
+      <div className="p-24 grid bg-hero-wave bg-center bg-cover">
+        <div className="flex flex-row">
+          <div className="w-1/2">
+            <h1 className=" text-7xl font-bold mt-12">Divi Online Banking</h1>
+            <p className="mt-8">
+              Vitae consequat augue. Vivamus eget dolor vel quam condimentum
+              sodales in bibendum odio urna sit amet.
+            </p>
+            <button className="bg-primary uppercase text-white rounded-full px-10 py-4 text-sm mt-8 flex flex-row">
+              Join Apenia today
+            </button>
+          </div>
+          <div className="w-[50vw] flex justify-end">
+            <img
+              src="/images/online-banking-28.png"
+              alt="Apenia.com"
+              className="w-[90%]"
+            />
+          </div>
+        </div>
+        <div className="grid grid-cols-4 mt-3 gap-4">
+          <div className=" col-start-1 col-end-1 p-6 border border-slate-300 rounded-xl">
+            <img src="/images/wallet.png" alt="wallet" />
+            <h2 className="text-xl font-bold my-4">Donec sed nisi</h2>
+            <p>Quisque aliquet velit sit amet sem interdu</p>
+          </div>
+          <div className="  col-start-2 col-end-2 p-6 border border-slate-300 rounded-xl">
+            <img src="/images/wallet.png" alt="wallet" />
+            <h2 className="text-xl font-bold my-4">Donec sed nisi</h2>
+            <p>Quisque aliquet velit sit amet sem interdu</p>
+          </div>
+          <div className="col-start-3 col-end-5 p-6 border border-slate-300 rounded-xl">
+            <img src="/images/wallet.png" alt="wallet" />
+            <h2 className="text-xl font-bold my-4">Donec sed nisi</h2>
+            <p>Quisque aliquet velit sit amet sem interdu</p>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
--- apenia2024/src/sections/landing/hero.js
+++ apenia2024/src/sections/landing/hero.js
@@ -3,7 +3,7 @@ const Hero = () => {
     <div className="p-24 grid bg-hero-wave bg-center bg-cover">
       <div className="flex flex-row">
         <div className="w-1/2">
-          <h1 className=" text-7xl font-bold ">Divi Online Banking</h1>
+          <h1 className=" text-7xl font-bold mt-12">Divi Online Banking</h1>
           <p>
             Vitae consequat augue. Vivamus eget dolor vel quam condimentum
             sodales in bibendum odio urna sit amet.
@@ -12,15 +12,15 @@ const Hero = () => {
             Join Apenia today
           </button>
         </div>
-        <div className="w-1/2">
+        <div className="w-[20vw]">
           <img
             src="/images/online-banking-28.png"
             alt="Apenia.com"
-            className="h-12"
+            className="w-[80%]"
           />
         </div>
       </div>
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-row justify-between mt-3">
         <div>
           <img src="/images/wallet.png" alt="wallet" />
           <h2 className="text-xl font-bold my-8">Donec sed nisi</h2>
