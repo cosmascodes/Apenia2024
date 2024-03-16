2f9cc086c20309354c52a5d3e66cfc57088f5b7b
cosmascodes <56020081+cosmascodes@users.noreply.github.com>
2024-03-16 15:51:07 +0300

    Final for today

--- apenia2024/src/components/footer.js
+++ apenia2024/src/components/footer.js
@@ -25,147 +25,156 @@ const Footer = () => {
       </div>
       <div className="grid grid-cols-4 w-full place-items-center mt-14">
         <div>
-          <h2 className=" text-xl mb-6">Products</h2>
+          <h2 className=" text-xl mb-6 font-semibold">Company</h2>
           <ul>
-            <li>
-              <a href="#" className=" text-base mb-4 ">
-                Divi Theme
+            <li className=" mb-2 ">
+              <a href="#" className=" text-base">
+                Blog
               </a>
             </li>
-            <li>
-              <a href="#" className=" text-base mb-4 ">
-                Divi Theme
+            <li className=" mb-2 ">
+              <a href="#" className=" text-base">
+                Join Our Team
               </a>
             </li>
-            <li>
-              <a href="#" className=" text-base mb-4 ">
-                Divi Theme
-              </a>
-            </li>
-            <li>
-              <a href="#" className=" text-base mb-4 ">
-                Divi Theme
-              </a>
-            </li>
-            <li>
-              <a href="#" className=" text-base mb-4 ">
-                Divi Theme
-              </a>
-            </li>
-            <li>
-              <a href="#" className=" text-base  mb-4">
-                Divi Theme
+            <li className=" mb-2 ">
+              <a href="#" className=" text-base">
+                About Us
               </a>
             </li>
           </ul>
         </div>
         <div>
-          <h2 className=" text-xl mb-6">Products</h2>
+          <h2 className=" text-xl mb-6 font-semibold">Help</h2>
           <ul>
-            <li>
-              <a href="#" className=" text-base mb-4 ">
-                Divi Theme
+            <li className=" mb-2 ">
+              <a href="#" className=" text-base">
+                Get Help
               </a>
             </li>
-            <li>
-              <a href="#" className=" text-base mb-4 ">
-                Divi Theme
+            <li className=" mb-2 ">
+              <a href="#" className=" text-base">
+                FAQs
               </a>
             </li>
-            <li>
-              <a href="#" className=" text-base mb-4 ">
-                Divi Theme
-              </a>
-            </li>
-            <li>
-              <a href="#" className=" text-base mb-4 ">
-                Divi Theme
-              </a>
-            </li>
-            <li>
-              <a href="#" className=" text-base mb-4 ">
-                Divi Theme
-              </a>
-            </li>
-            <li>
-              <a href="#" className=" text-base  mb-4">
-                Divi Theme
+            <li className=" mb-2 ">
+              <a href="#" className=" text-base">
+                Contact Us
               </a>
             </li>
           </ul>
         </div>
         <div>
-          <h2 className=" text-xl mb-6">Products</h2>
+          <h2 className=" text-xl mb-6 font-semibold">Customer</h2>
           <ul>
-            <li>
-              <a href="#" className=" text-base mb-4 ">
-                Divi Theme
+            <li className=" mb-2 ">
+              <a href="#" className=" text-base">
+                Save
               </a>
             </li>
-            <li>
-              <a href="#" className=" text-base mb-4 ">
-                Divi Theme
+            <li className=" mb-2 ">
+              <a href="#" className=" text-base">
+                Transfer & Spend
               </a>
             </li>
-            <li>
-              <a href="#" className=" text-base mb-4 ">
-                Divi Theme
-              </a>
-            </li>
-            <li>
-              <a href="#" className=" text-base mb-4 ">
-                Divi Theme
-              </a>
-            </li>
-            <li>
-              <a href="#" className=" text-base mb-4 ">
-                Divi Theme
-              </a>
-            </li>
-            <li>
-              <a href="#" className=" text-base  mb-4">
-                Divi Theme
+            <li className=" mb-2 ">
+              <a href="#" className=" text-base">
+                Invest
               </a>
             </li>
           </ul>
         </div>
         <div>
-          <h2 className=" text-xl mb-6">Products</h2>
+          <h2 className=" text-xl mb-6 font-semibold">Invest in Business</h2>
           <ul>
-            <li>
-              <a href="#" className=" text-base mb-4 ">
-                Divi Theme
+            <li className=" mb-2 ">
+              <a href="#" className=" text-base">
+                Become an Investor
               </a>
             </li>
-            <li>
-              <a href="#" className=" text-base mb-4 ">
-                Divi Theme
+            <li className=" mb-2 ">
+              <a href="#" className=" text-base">
+                Terms & Conditions
               </a>
             </li>
-            <li>
-              <a href="#" className=" text-base mb-4 ">
-                Divi Theme
-              </a>
-            </li>
-            <li>
-              <a href="#" className=" text-base mb-4 ">
-                Divi Theme
-              </a>
-            </li>
-            <li>
-              <a href="#" className=" text-base mb-4 ">
-                Divi Theme
-              </a>
-            </li>
-            <li>
-              <a href="#" className=" text-base  mb-4">
-                Divi Theme
+            <li className=" mb-2 ">
+              <a href="#" className=" text-base">
+                Privacy Policy
               </a>
             </li>
           </ul>
         </div>
       </div>
-      <div className="pt-24 pb-10">
+      <div className="w-full px-24 pt-12 flex flex-row gap-12">
+        <div>
+          <h2 className=" text-xl mb-6 font-semibold">Contact</h2>
+          <ul>
+            <li className=" mb-2 ">
+              <a href="#" className=" text-base">
+                +2547 14 991 111
+              </a>
+            </li>
+            <li className=" mb-2 ">
+              <a href="#" className=" text-base">
+                info@apenia.com
+              </a>
+            </li>
+          </ul>
+          <div className="flex flex-row">
+            <a href="#" className="p-2 bg-slate-200 mr-5 mt-5 rounded-full">
+              <img
+                src="/images/twitter.png"
+                alt="social icon"
+                className="h-[25px] w-[25px]"
+              />
+            </a>
+            <a href="#" className="p-2 bg-slate-200 mr-5 mt-5 rounded-full">
+              <img
+                src="/images/instagram.png"
+                alt="social icon"
+                className="h-[25px] w-[25px]"
+              />
+            </a>
+            <a href="#" className="p-2 bg-slate-200 mr-5 mt-5 rounded-full">
+              <img
+                src="/images/facebook.png"
+                alt="social icon"
+                className="h-[25px] w-[25px]"
+              />
+            </a>
+            <a href="#" className="p-2 bg-slate-200 mr-5 mt-5 rounded-full">
+              <img
+                src="/images/email.png"
+                alt="social icon"
+                className="h-[25px] w-[25px]"
+              />
+            </a>
+            <a href="#" className="p-2 bg-slate-200 mr-5 mt-5 rounded-full">
+              <img
+                src="/images/linkedIn.png"
+                alt="social icon"
+                className="h-[25px] w-[25px]"
+              />
+            </a>
+          </div>
+        </div>
+        <div>
+          <h2 className=" text-xl mb-6 font-semibold">Nairobi</h2>
+          <ul>
+            <li className=" mb-2 ">
+              <a href="#" className=" text-base">
+                Chandaria Incubation and innovation Center,
+              </a>
+            </li>
+            <li className=" mb-2 ">
+              <a href="#" className=" text-base">
+                Thika Road
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="pt-20 pb-10">
         <p>&copy; 2024 Apenia Technologies Ltd. All rights reserved.</p>
       </div>
     </footer>
