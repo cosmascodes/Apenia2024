6db4c539af4238662c7b676d4c661e9546e95ea5
cosmascodes <56020081+cosmascodes@users.noreply.github.com>
2024-03-17 21:18:02 +0300

    push header

--- apenia2024/src/components/header.js
+++ apenia2024/src/components/header.js
@@ -2,7 +2,11 @@ const Header = () => {
   return (
     <header className="fixed top-0 z-50 left-0 w-full h-20 bg-secondary px-6 overflow-hidden flex justify-between">
       <div className="py-6 h-full flex items-center">
-        <img src="/images/logo-a.svg" alt="Apenia.com" className="h-12" />
+        <img
+          src="/images/logo-a.svg"
+          alt="Apenia.com"
+          className="h-8 md:h-12"
+        />
       </div>
       <nav className="hidden h-full ">
         <ul className="text-black flex h-full items-center font-bold">
